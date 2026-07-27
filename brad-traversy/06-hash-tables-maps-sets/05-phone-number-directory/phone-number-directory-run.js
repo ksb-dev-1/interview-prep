6a8fb439d9b25d3d1a6bdd751407e74751c95a8b
ksb-dev-1 <ksbabaleshwar@gmail.com>
2026-07-27 22:33:11 +0530

    initial commit

--- brad-traversy/06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory-run.js
+++ brad-traversy/06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory-run.js
@@ -1,11 +1,9 @@
-const phoneNumberDirectory = require('./phone-number-directory');
+import { phoneNumberDirectory } from "./phone-number-directory.js";
 
 const phoneNumbers = [
-  'John:123-456-7890',
-  'Jane:987-654-3210',
-  'Joe:555-555-5555',
+  "John:123-456-7890",
+  "Jane:987-654-3210",
+  "Joe:555-555-5555",
 ];
 
 const result = phoneNumberDirectory(phoneNumbers);
-
-console.log(result.get('John'));
--- brad-traversy/06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
+++ brad-traversy/06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
@@ -1,3 +1,12 @@
-function phoneNumberDirectory() {}
+export function phoneNumberDirectory(arr) {
+  const map = new Map();
 
-module.exports = phoneNumberDirectory;
+  for (let i = 0; i < arr.length; i++) {
+    const entry = arr[i].split(":");
+    const key = entry[0];
+    const value = entry[1];
+
+    map.set(key, value);
+  }
+  return map;
+}
