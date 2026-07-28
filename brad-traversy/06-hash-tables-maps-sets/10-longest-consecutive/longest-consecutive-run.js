4d4b8694b3db68a8e9cb1e6cb639d7c55a05a0fd
ksb-dev-1 <ksbabaleshwar@gmail.com>
2026-07-28 10:18:50 +0530

    initial commit

--- brad-traversy/06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive-run.js
+++ brad-traversy/06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive-run.js
@@ -1,5 +1,5 @@
 import { longestConsecutiveSequence } from "./longest-consecutive.js";
 
-const result = longestConsecutiveSequence([100, 4, 200, 1, 3, 2]);
+const result = longestConsecutiveSequence([0, 3, 7, 2, 5, 8, 4, 6, 9, 1]);
 
 console.log(result);
--- brad-traversy/06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive.js
+++ brad-traversy/06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive.js
@@ -1,1 +1,43 @@
-export function longestConsecutiveSequence(arr) {}
+// export function longestConsecutiveSequence(arr) {
+//   if (arr.length === 0) return 0;
+
+//   //   const sorted = arr.sort((a, b) => a - b); // mutates original
+//   const sorted = [...arr].sort((a, b) => a - b); // doesn't mutate original
+//   let count = 1;
+//   let maxCount = 1;
+
+//   for (let i = 0; i < sorted.length - 1; i++) {
+//     if (sorted[i + 1] - sorted[i] === 1) {
+//       count++;
+//     } else if (sorted[i + 1] - sorted[i] === 0) {
+//       continue;
+//     } else if (sorted[i + 1] - sorted[i] > 1) {
+//       count = 1;
+//     }
+//     if (count > maxCount) {
+//       maxCount = count;
+//     }
+//   }
+//   return maxCount;
+// }
+
+export function longestConsecutiveSequence(arr) {
+  if (arr.length === 0) return 0;
+
+  const set = new Set(arr);
+  let longest = 0;
+
+  for (const num of set) {
+    if (!set.has(num - 1)) {
+      let current = num;
+      let length = 1;
+
+      while (set.has(current + 1)) {
+        current++;
+        length++;
+      }
+      longest = Math.max(longest, length);
+    }
+  }
+  return longest;
+}
